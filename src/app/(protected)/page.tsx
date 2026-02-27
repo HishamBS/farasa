1721aa6027eb97cc0fdf5f01addc6f23e092f5cb
Hisham Bin Seddeq <hbinseddeq@scai.sa>
2026-02-27 21:26:03 +0300

    Complete static compliance file structure and UI module set

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 import { ROUTES } from '@/config/routes'
 
-export default function Home() {
+export default function ProtectedRootPage() {
   redirect(ROUTES.CHAT)
 }
